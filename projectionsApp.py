92e210ae98dcd8367b2f0e3cccc27f883761d4e5
jjcabrera20 <jjcabrera20@yahoo.com.mx>
2022-08-01 17:09:23 -0500

    page setup

---
+++ projectionsApp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title='Population projection QC app R4V', page_icon=':chart_with_upwards_trend:', layout='wide')
+
+# Header
+st.subheader('Web application for quality control population projection data RMRP 2023/2024')
+st.title('QC Projection data RMRP 2023/2024')
+st.write('This app will hel to develop a QC to population projections per country RMRP')
+st.write('[learn more>](https://r4v.info)')
