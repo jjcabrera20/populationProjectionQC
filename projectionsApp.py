77ffa8380d55d6d92f1f25850eb0984f061cb135
jjcabrera20 <jjcabrera20@yahoo.com.mx>
2022-08-01 21:30:38 -0500

    page setup

--- projectionsApp.py
+++ projectionsApp.py
@@ -33,9 +33,7 @@ with st.container():
         st.file_uploader(label='Load data from excel template',accept_multiple_files=False)
     with right_column:
         st_lottie(animation, height=300, key='coding')
-os.remove('/app/populationprojectionqc/temp.xls')
+
 file = os.getcwd() + '/Template_Population_projections_2023-24.xlsx'
-st.write(os.getcwd())
-st.write(os.listdir(os.getcwd()))
-a=pd.read_excel(file)
+a=pd.read_excel(file,engine='openpyxl',)
 st.write(a)
