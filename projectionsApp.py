837780629b9d6d95b16fd412cf811e030cde69ef
jjcabrera20 <jjcabrera20@yahoo.com.mx>
2022-08-01 19:35:40 -0500

    page setup

--- projectionsApp.py
+++ projectionsApp.py
@@ -1,7 +1,17 @@
+import requests
 import streamlit as st
-
+from streamlit_lottie import st_lottie
 st.set_page_config(page_title='Population projection QC app R4V', page_icon=':chart_with_upwards_trend:', layout='wide')
+# Load assets
 
+def load_lottie_animation(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+animation = load_lottie_animation('https://assets2.lottiefiles.com/packages/lf20_16nllt42.json')
 # Header
 with st.container():
     st.subheader('Web application for quality control population projection data RMRP 2023/2024')
@@ -16,4 +26,4 @@ with st.container():
         st.header('Cargar datos')
         st.file_uploader(label='Load data from excel template',accept_multiple_files=False)
     with right_column:
-        st.header('Cargar datos 2')
+        st_lottie(animation, height=300, key='coding')
